43978818ae08d54213995cae1231ecb330839aa2
Eric Charles <eric@datalayer.io>
2023-09-01 15:43:40 +0200

    chore: bump jupyter-react

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import Script from 'next/script'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/require.js/2.3.4/require.min.js"  />
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/require.js/2.3.4/require.min.js" />
       </head>
       <body className={inter.className}>{children}</body>
     </html>
