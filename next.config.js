488d008ca9a1a226048a16cc9277f11fd7f5818a
Eric Charles <eric@datalayer.io>
2023-09-19 07:38:46 +0200

    chore: add output: 'export' config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // `next build` output will be in the `out` folder.
+  // see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
+  output: 'export',
   reactStrictMode: false,
 }
 
