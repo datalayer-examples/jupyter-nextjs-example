64cac0c25b67e2a761c29895720283c7c04d82e0
echarles <eric@datalayer.io>
2024-01-20 17:02:48 +0100

    fix: lite loading

--- next.config.js
+++ next.config.js
@@ -1,9 +1,40 @@
+/*
+ * Copyright (c) 2021-2023 Datalayer, Inc.
+ *
+ * MIT License
+ */
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // `next build` output will be in the `out` folder.
-  // see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
-  output: 'export',
   reactStrictMode: false,
+  webpack: (config, options) => {
+    config.module.rules.push(
+      // Ship the JupyterLite service worker.
+      {
+        resourceQuery: /text/,
+        type: 'asset/resource',
+        generator: {
+          filename: '[name][ext]',
+        },
+      },
+      // Rule for pyodide kernel
+      {
+        test: /pypi\/.*/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'pypi/[name][ext][query]',
+        },
+      },
+      {
+        test: /pyodide-kernel-extension\/schema\/.*/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'schema/[name][ext][query]',
+        },
+      },
+    )
+    return config
+  },
 }
 
 module.exports = nextConfig
