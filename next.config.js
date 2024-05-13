3025618c9e3e6167462e18f2328fe361ce61e278
Eric Charles <eric@datalayer.io>
2024-05-13 10:37:17 +0200

    fix: support jupyterlab svg

--- next.config.js
+++ next.config.js
@@ -9,6 +9,23 @@ const nextConfig = {
   reactStrictMode: false,
   webpack: (config, options) => {
     config.module.rules.push(
+      { test: /\.js.map$/, type: 'asset/resource' },
+      {
+        // In .css files, svg is loaded as a data URI.
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        issuer: /\.css$/,
+        use: {
+          loader: 'svg-url-loader',
+          options: { encoding: 'none', limit: 10000 },
+        },
+      },
+      {
+        // In .ts and .tsx files (both of which compile to .js), svg files
+        // must be loaded as a raw string instead of data URIs.
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        issuer: /\.js$/,
+        type: 'asset/source',
+      },
       // Ship the JupyterLite service worker.
       {
         resourceQuery: /text/,
